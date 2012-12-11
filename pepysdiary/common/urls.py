61632d9e9dfe11933210fae4c3fc7895a321fb39
Phil Gyford <phil@gyford.com>
2012-12-11 12:47:59 +0000

    Add redirects from old MT URLs

--- pepysdiary/common/urls.py
+++ pepysdiary/common/urls.py
@@ -1,12 +1,27 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import RedirectView
+
+from pepysdiary.common.views import DiaryEntryRedirectView
+
+
 admin.autodiscover()
 
+# Redirects from old Movable Type URLs to new ones.
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'pepysdiary.views.home', name='home'),
+    # Redirect from old main /archive/ page.
+    url(r'^archive/$', RedirectView.as_view(
+                                    url=reverse_lazy('diary_entry_archive'))),
 
+    # Redirect from old /archive/1660/01/01/index.php URLs:
+    url(r'^archive/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(index\.php)?$',
+        DiaryEntryRedirectView.as_view()
+    ),
+)
+
+# The main URL conf for actual pages, not redirects.
+urlpatterns += patterns('',
     url(r'^diary/', include('pepysdiary.diary.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
--- pepysdiary/common/views.py
+++ pepysdiary/common/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.core.urlresolvers import reverse
+from django.views.generic import RedirectView
+
+
+class DiaryEntryRedirectView(RedirectView):
+    """
+    To help with redirecting from old /archive/1660/01/01/index.php URLs to the
+    new Diary Entry URLs.
+    """
+    def get_redirect_url(self, year, month, day):
+        return reverse('diary_entry', kwargs={
+            'year': year, 'month': month, 'day': day})
