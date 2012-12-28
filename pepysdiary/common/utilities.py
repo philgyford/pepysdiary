ddc972444ef284a072a8658c26ff2b770943f08d
Phil Gyford <phil@gyford.com>
2012-12-28 18:24:40 +0000

    Add static files images for In Depth, and fix links to them

--- pepysdiary/common/utilities.py
+++ pepysdiary/common/utilities.py
@@ -8,25 +8,30 @@ def fix_old_links(text):
     """
     # From pepysdiary.com/p/42.php
     # to   pepysdiary.com/encyclopedia/42/
-    text = re.sub(r'pepysdiary.com\/p\/(\d+)\.php',
+    text = re.sub(r'pepysdiary\.com\/p\/(\d+)\.php',
                     r'pepysdiary.com/encyclopedia/\1/',
                     text)
     # From pepysdiary.com/indepth/archive/2012/12/23/slug.php
     # to   pepysdiary.com/indepth/2012/12/23/slug/
-    text = re.sub(r'pepysdiary.com\/indepth\/archive\/(.*?)\.php',
+    text = re.sub(r'pepysdiary\.com\/indepth\/archive\/(.*?)\.php',
                     r'pepysdiary.com/indepth/\1/',
                     text)
     # From pepysdiary.com/archive/1666/12/23/
     # or   pepysdiary.com/archive/1666/12/23/index.php
     # to   pepysdiary.com/diary/1666/12/23/
-    text = re.sub(r'pepysdiary.com\/archive\/(\d\d\d\d\/\d\d\/\d\d)\/(index\.php)?',
+    text = re.sub(r'pepysdiary\.com\/archive\/(\d\d\d\d\/\d\d\/\d\d)\/(index\.php)?',
                     r'pepysdiary.com/diary/\1/',
                     text)
     # From pepysdiary.com/letters/1666/12/23/pepys-to-evelyn.pyp
     # to   pepysdiary.com/letters/1666/12/23/pepys-to-evelyn/
-    text = re.sub(r'pepysdiary.com\/letters\/(\d\d\d\d\/\d\d\/\d\d\/[\w-]+)\.php',
-            r'pepysdiary.com/letters/\1/',
-            text)
+    text = re.sub(r'pepysdiary\.com\/letters\/(\d\d\d\d\/\d\d\/\d\d\/[\w-]+)\.php',
+                    r'pepysdiary.com/letters/\1/',
+                    text)
+    # From pepysdiary.com/indepth/images/2012/05/31/SamuelPepys_1666.jpg
+    # to   pepysdiary.com/static/img/indepth/2012/05/31/SamuelPepys_1666.jpg
+    text = re.sub(r'pepysdiary\.com\/indepth\/images\/(.*?\.(?:jpg|png|gif))',
+                    r'pepysdiary.com/static/img/indepth/\1',
+                    text)
     return text
 
 
