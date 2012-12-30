aa2aebbf62fa73e643553a1886e0cc65a17cead2
Phil Gyford <phil@gyford.com>
2012-12-30 18:50:53 +0000

    Add Sendgrid Email settings

--- pepysdiary/settings/heroku.py
+++ pepysdiary/settings/heroku.py
@@ -19,6 +19,11 @@ CACHES = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME')
+EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
