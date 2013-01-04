b17c7bdd26dd8c51dd5f73fc062ad805afdb7199
Phil Gyford <phil@gyford.com>
2013-01-04 17:56:17 +0000

    Remove news and indepth from static files dirs

--- pepysdiary/settings/defaults.py
+++ pepysdiary/settings/defaults.py
@@ -56,8 +56,6 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(PROJECT_ROOT, 'common', 'static'),
-    os.path.join(PROJECT_ROOT, 'indepth', 'static'),
-    os.path.join(PROJECT_ROOT, 'news', 'static'),
 )
 
 # List of finder classes that know how to find static files in
