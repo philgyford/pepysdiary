cac62ff9018db1ae6b673201ed5215645bfbcef0
Phil Gyford <phil@gyford.com>
2013-02-01 11:42:29 +0000

    Change format of visit cookie values

--- pepysdiary/common/middleware.py
+++ pepysdiary/common/middleware.py
@@ -1,8 +1,7 @@
 # coding: utf-8
+import calendar
 import datetime
-import dateutil.parser
-
-from django.utils import timezone
+import pytz
 
 
 class VisitTimeMiddleware(object):
@@ -52,21 +51,23 @@ class VisitTimeMiddleware(object):
         last_view_cookie = request.COOKIES.get('last_view', '')
         if last_view_cookie != '':
             try:
-                self.last_view = dateutil.parser.parse(last_view_cookie)
+                self.last_view = self.cookie_value_to_datetime(
+                                                            last_view_cookie)
             except:
                 pass
 
         visit_start_cookie = request.COOKIES.get('visit_start', '')
         if visit_start_cookie != '':
             try:
-                self.visit_start = dateutil.parser.parse(visit_start_cookie)
+                self.visit_start = self.cookie_value_to_datetime(
+                                                            visit_start_cookie)
             except:
                 pass
 
         prev_visit_end_cookie = request.COOKIES.get('prev_visit_end', '')
         if prev_visit_end_cookie != '':
             try:
-                self.prev_visit_end = dateutil.parser.parse(
+                self.prev_visit_end = self.cookie_value_to_datetime(
                                                         prev_visit_end_cookie)
             except:
                 pass
@@ -75,30 +76,45 @@ class VisitTimeMiddleware(object):
     def _set_cookies(self, response):
         """Sets new cookie values."""
 
-        time_now = timezone.now()
+        time_now = datetime.datetime.now(pytz.utc)
 
         # What time will our cookies expire?
         cookie_expire = time_now + datetime.timedelta(self.cookie_duration)
 
         if self.last_view is None or self.visit_start is None:
             # User hasn't been here before.
-            response.set_cookie('visit_start', value=time_now.isoformat(),
-                                                        expires=cookie_expire)
+            response.set_cookie('visit_start',
+                                value=self.datetime_to_cookie_value(time_now),
+                                expires=cookie_expire)
 
         elif self.visit_start is not None:
             # User has viewed a page before.
             current_visit_duration = time_now - self.visit_start
             if current_visit_duration.total_seconds() > self.visit_length:
                 # This is a new visit for the user.
-                response.set_cookie('prev_visit_end',
-                                    value=self.last_view.isoformat(),
-                                    expires=cookie_expire)
-                response.set_cookie('visit_start',
-                                    value=time_now.isoformat(),
-                                    expires=cookie_expire)
+                response.set_cookie(
+                        'prev_visit_end',
+                        value=self.datetime_to_cookie_value(self.last_view),
+                        expires=cookie_expire)
+                response.set_cookie(
+                            'visit_start',
+                            value=self.datetime_to_cookie_value(time_now),
+                            expires=cookie_expire)
 
         response.set_cookie('last_view',
-                            value=time_now.isoformat(),
+                            value=self.datetime_to_cookie_value(time_now),
                             expires=cookie_expire)
 
         return response
+
+    def cookie_value_to_datetime(self, str):
+        """
+        Takes a string from a cookie, a unix timestamp, and returns a
+        datetime object, UTC.
+        """
+        naive_dt = datetime.datetime.fromtimestamp(int(str))
+        return naive_dt.replace(tzinfo=pytz.utc)
+
+    def datetime_to_cookie_value(self, dt):
+        """Takes a datetime object and returns a UTC unix timestamp."""
+        return calendar.timegm(dt.timetuple())
