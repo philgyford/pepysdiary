ab51489cce2591ec24bf726dc74b57c2b4fa0322
Phil Gyford <phil@gyford.com>
2013-02-20 16:30:28 +0000

    Re-order classes

--- pepysdiary/news/views.py
+++ pepysdiary/news/views.py
@@ -7,50 +7,17 @@ from pepysdiary.common.views import BaseRSSFeed
 from pepysdiary.news.models import Post
 
 
-class PostDetailView(DateDetailView):
+class PostArchiveView(ListView):
     """
-    Note: This generates Runtime Errors because we're using a DateTime field
-    (date_published) rather than a Date field, and this ends up using
-    naive datetimes.
-    https://code.djangoproject.com/ticket/18794
-    https://docs.djangoproject.com/en/dev/ref/class-based-views/mixins-date-based/#datemixin
+    The front page of the Site News section.
     """
     model = Post
-    date_field = 'date_published'
     queryset = Post.published_posts.all()
-    year_format = '%Y'
-    month_format = '%m'
-    day_format = '%d'
 
     def get_context_data(self, **kwargs):
-        context = super(PostDetailView, self).get_context_data(**kwargs)
+        context = super(PostArchiveView, self).get_context_data(**kwargs)
         context['categories'] = Post.CATEGORY_CHOICES
-        extra_context = self.get_next_previous()
-        context.update(extra_context)
         return context
-
-    def get_next_previous(self):
-        """
-        Get the next/previous Posts based on the current Post's date.
-        """
-        try:
-            previous_post = self.model.published_posts.filter(
-                                date_published__lt=self.object.date_published
-                            ).order_by('-date_published')[:1].get()
-        except self.model.DoesNotExist:
-            previous_post = None
-
-        try:
-            next_post = self.model.published_posts.filter(
-                                date_published__gt=self.object.date_published
-                            ).order_by('date_published')[:1].get()
-        except self.model.DoesNotExist:
-            next_post = None
-
-        return {
-            'previous_post': previous_post,
-            'next_post': next_post,
-        }
 
 
 class PostCategoryArchiveView(ListView):
@@ -96,17 +63,50 @@ class PostCategoryArchiveView(ListView):
         return context
 
 
-class PostArchiveView(ListView):
+class PostDetailView(DateDetailView):
     """
-    The front page of the Site News section.
+    Note: This generates Runtime Errors because we're using a DateTime field
+    (date_published) rather than a Date field, and this ends up using
+    naive datetimes.
+    https://code.djangoproject.com/ticket/18794
+    https://docs.djangoproject.com/en/dev/ref/class-based-views/mixins-date-based/#datemixin
     """
     model = Post
+    date_field = 'date_published'
     queryset = Post.published_posts.all()
+    year_format = '%Y'
+    month_format = '%m'
+    day_format = '%d'
 
     def get_context_data(self, **kwargs):
-        context = super(PostArchiveView, self).get_context_data(**kwargs)
+        context = super(PostDetailView, self).get_context_data(**kwargs)
         context['categories'] = Post.CATEGORY_CHOICES
+        extra_context = self.get_next_previous()
+        context.update(extra_context)
         return context
+
+    def get_next_previous(self):
+        """
+        Get the next/previous Posts based on the current Post's date.
+        """
+        try:
+            previous_post = self.model.published_posts.filter(
+                                date_published__lt=self.object.date_published
+                            ).order_by('-date_published')[:1].get()
+        except self.model.DoesNotExist:
+            previous_post = None
+
+        try:
+            next_post = self.model.published_posts.filter(
+                                date_published__gt=self.object.date_published
+                            ).order_by('date_published')[:1].get()
+        except self.model.DoesNotExist:
+            next_post = None
+
+        return {
+            'previous_post': previous_post,
+            'next_post': next_post,
+        }
 
 
 class LatestPostsFeed(BaseRSSFeed):
