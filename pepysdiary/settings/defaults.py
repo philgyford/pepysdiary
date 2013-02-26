f5bc410f68b0e19f8581ccb1006ad5df292124d2
Phil Gyford <phil@gyford.com>
2013-02-26 16:57:50 +0000

    Redirect from *.herokuapp.com to the correct domain

--- pepysdiary/settings/defaults.py
+++ pepysdiary/settings/defaults.py
@@ -84,6 +84,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'pepysdiary.common.middleware.VisitTimeMiddleware',
+    'pepysdiary.common.middleware.HerokuDomainRedirectMiddleware',
     # Must be last:
     'django.middleware.cache.FetchFromCacheMiddleware',
 )
