5979d973cc3bfce6001678f160aa9c52460c5587
Phil Gyford <phil@gyford.com>
2013-03-20 11:23:08 +0000

    Added On This Day data. Fixes #24

--- pepysdiary/events/templatetags/event_tags.py
+++ pepysdiary/events/templatetags/event_tags.py
@@ -1,13 +1,69 @@
 # coding: utf-8
 from django import template
+from django.core.urlresolvers import reverse
 
+from pepysdiary.diary.models import Entry
 from pepysdiary.events.models import DayEvent
+from pepysdiary.letters.models import Letter
 
 register = template.Library()
 
 
-@register.simple_tag
-def events_for_day(date, exclude=None):
+def events_html(title, event_list):
+    """
+    Generates the HTML for a list of Diary Entries, Letters or DayEvents.
+    `title` is for the <h3>, eg 'Diary Entries' or 'In Parliament'.
+    `event_list` is a list of dicts, each dict having a `text` element and an
+    optional `url`.
+    """
+    if len(event_list) == 0:
+        return ''
+    html = u"<h3>%s</h3>\n<ul>\n" % title
+    for li in event_list:
+        if 'url' in li and li['url'] != '':
+            html += u"<li><a href=\"%s\">%s</a></li>\n" % (
+                                                        li['url'], li['text'])
+        else:
+            html += u"<li>%s</li>\n" % li['text']
+    html += "</ul>\n"
+    return html
+
+
+def entries_for_day(date):
+    """
+    Returns HTML for links to any Diary Entries that were written on `date`.
+    Or an empty string if there aren't any.
+    """
+    event_list = []
+    entries = Entry.objects.filter(diary_date=date)
+    if len(entries) > 0:
+        for entry in entries:
+            event_list.append({'url': entry.get_absolute_url(),
+                                'text': entry.title})
+    return events_html('Diary Entries', event_list)
+
+
+def letters_for_day(date):
+    """
+    Returns HTML for links to any Letters that were written on `date`.
+    Or an empty string if there aren't any.
+    """
+    event_list = []
+    letters = Letter.objects.filter(letter_date=date)
+    if len(letters) > 0:
+        for letter in letters:
+            event_list.append({'url': letter.get_absolute_url(),
+                                'text': letter.title})
+    return events_html('Letters', event_list)
+
+
+def dayevents_for_day(date):
+    """
+    Returns HTML for displaying any DayEvents that happen on `date`.
+    Or an empty string if there aren't any.
+    """
+    html = ''
+
     sources = {key: label for (key, label) in DayEvent.SOURCE_CHOICES}
 
     # Makes a dict like:
@@ -21,31 +77,44 @@ def events_for_day(date, exclude=None):
             events_by_source[ev.source][ev.title] = []
         events_by_source[ev.source][ev.title].append(ev)
 
-    html = ''
     for source_key, titles in events_by_source.items():
         if len(titles) > 0:
-            # eg: '<h3>In Parliament</h3>'.
-            html += u'<h3>%s</h3><ul>' % sources[source_key]
-
+            event_list = []
             for title, events in titles.items():
                 # So, title is the string that all the events share.
                 # events is now a list of DayEvent objects.
                 if len(events) == 1:
                     # Only one event, so just list it.
-                    if events[0].url == '':
-                        html += u'<li>%s</li>' % (title)
-                    else:
-                        html += u'<li><a href="%s">%s</a></li>' % (
-                                                        events[0].url, title)
+                    event_list.append({'url': events[0].url,
+                                        'text': events[0].title})
                 else:
                     # Many events with the same title. So show numbered links.
-                    html += u'<li>%s: ' % title
+                    event_html = u'%s: ' % title
                     for n, event in enumerate(events):
-                        html += u'<a href="%s">%s</a> ' % (event.url, (n + 1))
-                    html += '</li>'
+                        event_html += u'<a href="%s">%s</a> ' % (
+                                                            event.url, (n + 1))
+                    event_list.append({'text': event_html})
 
-            html += '</ul>'
+            # Make the HTML. sources[source_key] is like 'In Parliament'.
+            html += events_html(sources[source_key], event_list)
 
+    if html != '':
+        html += '<p class="text-right"><small><a href="%s#on-this-day">About these events</a></small></p>' % (
+            reverse('django.contrib.flatpages.views.flatpage',
+                    kwargs={'url': '/about/text/'})
+        )
+
+    return html
+
+
+@register.simple_tag
+def events_for_day(date, exclude=None):
+    html = ''
+    if exclude != 'entries':
+        html += entries_for_day(date)
+    if exclude != 'letters':
+        html += letters_for_day(date)
+    html += dayevents_for_day(date)
     return html
 
 
