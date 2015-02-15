84986843e2055a8e9a88a66062a7dafcb745dad5
Phil Gyford <phil@gyford.com>
2015-02-15 19:19:26 +0000

    Remove DJANGO_SETTINGS_MODULE env var from wsgi.py

--- pepysdiary/wsgi.py
+++ pepysdiary/wsgi.py
@@ -13,9 +13,6 @@ middleware here, or combine a Django application with an application of another
 framework.
 
 """
-import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pepysdiary.settings.heroku")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
