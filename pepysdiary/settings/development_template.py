e230d021b2a699090324fca1f537164eb54f7a8d
Phil Gyford <phil@gyford.com>
2015-05-17 20:23:39 +0100

    Remove s3utils

--- pepysdiary/settings/development_template.py
+++ pepysdiary/settings/development_template.py
@@ -57,26 +57,9 @@ MT_MYSQL_DB_PASSWORD = ''
 MT_MYSQL_DB_HOST = ''
 
 
-######################################################################
-# Uncomment these to use S3 for storage:
-
-# DEFAULT_FILE_STORAGE = 'pepysdiary.common.s3utils.MediaS3BotoStorage'
-# STATICFILES_STORAGE = 'pepysdiary.common.s3utils.StaticS3BotoStorage'
-
-# AWS_ACCESS_KEY_ID = ''
-# AWS_SECRET_ACCESS_KEY = ''
-# AWS_STORAGE_BUCKET_NAME = ''
-
-# S3_URL = 'https://%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-# # Store static and media files in separate directories:
-# STATIC_URL = S3_URL + STATIC_URL
-# MEDIA_URL = S3_URL + MEDIA_URL
-
-
 #############################################################################
 # PEPYSDIARY-SPECIFIC SETTINGS.
 
-GOOGLE_MAPS_API_KEY = ''
 GOOGLE_ANALYTICS_ID = ''
 
 # From https://www.google.com/recaptcha/
