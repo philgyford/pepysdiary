5d3d262b83126be27dba4bc9ea3690357f55b3fe
Phil Gyford <phil@gyford.com>
2016-03-15 17:14:02 +0000

    Add staging settings

---
+++ pepysdiary/settings/staging.py
@@ -0,0 +1,98 @@
+from defaults import *
+from os import environ
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('Phil Gyford', 'phil@gyford.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': environ.get('DB_NAME'),
+        'USER': environ.get('DB_USERNAME'),
+        'PASSWORD': environ.get('DB_PASSWORD'),
+        'HOST': environ.get('DB_HOST'),
+        'PORT': '',
+    }
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = environ.get('SENDGRID_USERNAME')
+EMAIL_HOST_PASSWORD = environ.get('SENDGRID_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+# If you *don't* want to prepend www to the URL, remove the setting from
+# the environment entirely. Otherwise, set to 'True' (or anything tbh).
+PREPEND_WWW = True
+
+ALLOWED_HOSTS = environ.get('ALLOWED_HOSTS', '*').split(',')
+
+CACHES = {
+  'default': {
+    'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
+    'LOCATION': '127.0.0.1:11211',
+    'TIMEOUT': 500, # milliseconds
+  }
+}
+
+# Make this unique, and don't share it with anybody.
+# http://www.miniwebtool.com/django-secret-key-generator/
+SECRET_KEY = environ.get('SECRET_KEY', '')
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
+#############################################################################
+# PEPYSDIARY-SPECIFIC SETTINGS.
+
+GOOGLE_ANALYTICS_ID = 'UA-89135-2'
+
+# From https://www.google.com/recaptcha/
+RECAPTCHA_PUBLIC_KEY = environ.get('RECAPTCHA_PUBLIC_KEY')
+RECAPTCHA_PRIVATE_KEY = environ.get('RECAPTCHA_PRIVATE_KEY')
+RECAPTCHA_USE_SSL = True
+
+# Do we use Akismet/TypePad spam checking?
+# True/False. If false, no posted comments are checked.
+# If True, AKISMET_API_KEY must also be set.
+USE_SPAM_CHECK = True
+
+# From http://akismet.com/
+AKISMET_API_KEY = environ.get('AKISMET_API_KEY')
+
+# From http://mapbox.com/
+MAPBOX_MAP_ID = environ.get('MAPBOX_MAP_ID')
+MAPBOX_ACCESS_TOKEN = environ.get('MAPBOX_ACCESS_TOKEN')
