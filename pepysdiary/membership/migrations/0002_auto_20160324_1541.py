34167b941f563f5682865acb933b0d80c8786018
Phil Gyford <phil@gyford.com>
2016-03-24 15:42:35 +0000

    Ran makemigrations and got this....

---
+++ pepysdiary/membership/migrations/0002_auto_20160324_1541.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('membership', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='person',
+            name='groups',
+            field=models.ManyToManyField(related_query_name='user', related_name='user_set', to='auth.Group', blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', verbose_name='groups'),
+        ),
+        migrations.AlterField(
+            model_name='person',
+            name='last_login',
+            field=models.DateTimeField(null=True, verbose_name='last login', blank=True),
+        ),
+    ]
