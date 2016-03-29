b8df4828abf0b78b796c53126d59cbd9aab8eab6
Phil Gyford <phil@gyford.com>
2016-03-29 15:57:04 +0100

    Add new Password Validators, and use in registration form

--- pepysdiary/settings/defaults.py
+++ pepysdiary/settings/defaults.py
@@ -151,6 +151,24 @@ INSTALLED_APPS = [
     'pepysdiary.annotations',
 ]
 
+# Password validation
+# https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
 AUTH_USER_MODEL = 'membership.Person'
 
 LOGIN_REDIRECT_URL = 'home'
