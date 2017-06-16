c0516ef5433f4210c44b55fc801cb12ab5d0283b
Phil Gyford <phil@gyford.com>
2017-06-16 17:23:04 +0100

    Stop logging disallowed hosts errors.
    Fixes #146

--- pepysdiary/settings/production.py
+++ pepysdiary/settings/production.py
@@ -63,13 +63,20 @@ LOGGING = {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
-        }
+        },
+        'null': {
+            'class': 'logging.NullHandler',
+        },
     },
     'loggers': {
         'django.request': {
             'handlers': ['mail_admins'],
             'level': 'ERROR',
             'propagate': True,
+        },
+        'django.security.DisallowedHost': {
+            'handlers': ['null'],
+            'propagate': False,
         },
     }
 }
