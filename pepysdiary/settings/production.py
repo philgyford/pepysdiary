9047478281f37ada7e1cb22e9d9fcf2158ac035c
Phil Gyford <phil@gyford.com>
2017-09-11 10:39:41 +0100

    Turn on SSL for production

--- pepysdiary/settings/production.py
+++ pepysdiary/settings/production.py
@@ -100,6 +100,15 @@ LOGGING = {
     }
 }
 
+# https
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+# SECURE_HSTS_SECONDS = 3600
+# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+# SECURE_HSTS_PRELOAD = True
+
 
 #############################################################################
 # PEPYSDIARY-SPECIFIC SETTINGS.
