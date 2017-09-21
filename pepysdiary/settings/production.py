1c45ad14f40a566175d5ef4198d7198fd059b18e
Phil Gyford <phil@gyford.com>
2017-09-21 11:12:41 +0100

    Fix indentation

--- pepysdiary/settings/production.py
+++ pepysdiary/settings/production.py
@@ -31,37 +31,37 @@ environ['MEMCACHE_USERNAME'] = get_env_variable('MEMCACHIER_USERNAME')
 environ['MEMCACHE_PASSWORD'] = get_env_variable('MEMCACHIER_PASSWORD')
 
 CACHES = {
-  'default': {
-    'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
+    'default': {
+        'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
 
-    # Use binary memcache protocol (needed for authentication)
-    'BINARY': True,
+        # Use binary memcache protocol (needed for authentication)
+        'BINARY': True,
 
-    # TIMEOUT is not the connection timeout! It's the default expiration
-    # timeout that should be applied to keys! Setting it to `None`
-    # disables expiration.
-    'TIMEOUT': None,
+        # TIMEOUT is not the connection timeout! It's the default expiration
+        # timeout that should be applied to keys! Setting it to `None`
+        # disables expiration.
+        'TIMEOUT': None,
 
-    'OPTIONS': {
-        # Enable faster IO
-        'tcp_nodelay': True,
+        'OPTIONS': {
+            # Enable faster IO
+            'tcp_nodelay': True,
 
-        # Keep connection alive
-        'tcp_keepalive': True,
+            # Keep connection alive
+            'tcp_keepalive': True,
 
-        # Timeout settings
-        'connect_timeout': 2000, # ms
-        'send_timeout': 750 * 1000, # us
-        'receive_timeout': 750 * 1000, # us
-        '_poll_timeout': 2000, # ms
+            # Timeout settings
+            'connect_timeout': 2000, # ms
+            'send_timeout': 750 * 1000, # us
+            'receive_timeout': 750 * 1000, # us
+            '_poll_timeout': 2000, # ms
 
-        # Better failover
-        'ketama': True,
-        'remove_failed': 1,
-        'retry_timeout': 2,
-        'dead_timeout': 30,
+            # Better failover
+            'ketama': True,
+            'remove_failed': 1,
+            'retry_timeout': 2,
+            'dead_timeout': 30,
+        }
     }
-  }
 }
 
 # A sample logging configuration. The only tangible logging
