86f4dca8f48c13c26ab49ee67625e34bc2c34a72
Phil Gyford <phil@gyford.com>
2018-04-13 15:55:47 +0100

    Make API queries more efficient
    For related fields, like an Entry's Topics, or a Topic's Entries,
    Django REST Framework was fetching ALL of the objects' columns from the
    DB, when it only needed the `pk` or `diary_date` respectively.
    
    So, found a way to tell it to only fetch the relevant fields:
    https://stackoverflow.com/a/49819377/250962

--- pepysdiary/api/serializers.py
+++ pepysdiary/api/serializers.py
@@ -25,6 +25,34 @@ entries_kwargs = {
 }
 
 
+class TopicsMixin(object):
+    """
+    For Serializers that need to fetch a list of Topic URLs.
+
+    The Serializer should include:
+
+        topics = serializers.SerializerMethodField()
+    """
+
+    def get_topics(self, instance):
+        """
+        Returns a list of URLs to Topics' API detail views.
+        We're not using HyperlinkedRelatedField() because that fetches ALL of
+        the Topics' data from the database, which is a lot of unncessary stuff.
+        """
+        request = self.context.get('request')
+        topics = []
+        qs = instance.topics.values('pk').order_by('pk')
+
+        for topic in qs:
+            topics.append(reverse_lazy(
+                        topics_kwargs['view_name'],
+                        kwargs={topics_kwargs['lookup_url_kwarg']: topic['pk']},
+                        request=request))
+
+        return topics
+
+
 class BaseSerializer(serializers.ModelSerializer):
     """
     Parent class that includes a `webURL` field for the object's
@@ -65,17 +93,14 @@ class CategoryListSerializer(BaseSerializer):
                 )
 
 
-class CategoryDetailSerializer(CategoryListSerializer):
+class CategoryDetailSerializer(TopicsMixin, CategoryListSerializer):
     """
     Full info about the Category, for the DetailView.
 
     Includes a list of all Topics in the Category.
     """
 
-    topics = serializers.HyperlinkedRelatedField(
-                                            read_only=True,
-                                            many=True,
-                                            **topics_kwargs)
+    topics = serializers.SerializerMethodField()
 
     class Meta:
         model = Category
@@ -102,7 +127,7 @@ class EntryListSerializer(BaseSerializer):
                 )
 
 
-class EntryDetailSerializer(EntryListSerializer):
+class EntryDetailSerializer(TopicsMixin, EntryListSerializer):
     """
     Full info about an Entry for the DetailView.
 
@@ -116,10 +141,7 @@ class EntryDetailSerializer(EntryListSerializer):
     lastAnnotationTime = serializers.DateTimeField(
                                     source='last_comment_time', read_only=True)
 
-    topics = serializers.HyperlinkedRelatedField(
-                                            read_only=True,
-                                            many=True,
-                                            **topics_kwargs)
+    topics = serializers.SerializerMethodField()
 
     class Meta:
         model = Entry
@@ -171,11 +193,7 @@ class TopicDetailSerializer(TopicListSerializer):
                                         many=True,
                                         **categories_kwargs)
 
-    entries = serializers.HyperlinkedRelatedField(
-                                        source='diary_references',
-                                        read_only=True,
-                                        many=True,
-                                        **entries_kwargs)
+    entries = serializers.SerializerMethodField()
 
     wikipediaURL = serializers.URLField(source='wikipedia_url', read_only=True)
 
@@ -196,3 +214,21 @@ class TopicDetailSerializer(TopicListSerializer):
                     'categories', 'entries',
                     'apiURL', 'webURL',
                 )
+
+    def get_entries(self, instance):
+        """
+        Returns a list of URLs to Entries' API detail views.
+        We're not using HyperlinkedRelatedField() because that fetches ALL of
+        the Entries' data from the database, which is a lot of unncessary stuff.
+        """
+        request = self.context.get('request')
+        entries = []
+        qs = instance.diary_references.values('diary_date').order_by('diary_date')
+
+        for entry in qs:
+            entries.append(reverse_lazy(
+                entries_kwargs['view_name'],
+                kwargs={entries_kwargs['lookup_url_kwarg']: entry['diary_date']},
+                request=request))
+
+        return entries
