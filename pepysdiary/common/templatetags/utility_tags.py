daf7624319ad839cec3f99e1768fb10093e9f532
Phil Gyford <phil@gyford.com>
2019-03-25 10:50:00 +0000

    Maintain query strings when using pagination in template
    For #98

---
+++ pepysdiary/common/templatetags/utility_tags.py
@@ -0,0 +1,27 @@
+from django import template
+
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def query_string(context, key, value):
+    """
+    For adding/replacing a key=value pair to the GET string for a URL.
+
+    eg, if we're viewing ?p=3 and we do {% query_string order 'taken' %}
+    then this returns "p=3&order=taken"
+
+    And, if we're viewing ?p=3&order=uploaded and we do the same thing, we get
+    the same result (ie, the existing "order=uploaded" is replaced).
+
+    Expects the request object in context to do the above; otherwise it will
+    just return a query string with the supplied key=value pair.
+    """
+    try:
+        request = context['request']
+        args = request.GET.copy()
+    except KeyError:
+        args = QueryDict('').copy()
+    args[key] = value
+    return args.urlencode()
