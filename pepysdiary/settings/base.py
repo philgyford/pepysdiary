9fa80e162702f7968c1fdcdb76c0736e5790b867
Phil Gyford <phil@gyford.com>
2019-05-11 15:17:43 +0100

    Black formatting

--- pepysdiary/settings/base.py
+++ pepysdiary/settings/base.py
@@ -1,6 +1,6 @@
 # Should be extended by settings in, eg, production.py.
+from datetime import date, timedelta
 import os
-from os import environ
 
 from django.core.exceptions import ImproperlyConfigured
 
@@ -10,11 +10,11 @@ def get_env_variable(var_name):
     try:
         return os.environ[var_name]
     except KeyError:
-        error_msg = 'Set the {} environemnt variable.'.format(var_name)
+        error_msg = "Set the {} environemnt variable.".format(var_name)
         raise ImproperlyConfigured(error_msg)
 
 
-PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 DEBUG = True
 
@@ -22,11 +22,11 @@ DEBUG = True
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-gb'
+LANGUAGE_CODE = "en-gb"
 
 SITE_ID = 1
 
@@ -43,24 +43,24 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media/')
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, "media/")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = "/media/"
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static_collected/')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "static_collected/")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 # Additional locations of static files
 STATICFILES_DIRS = [
@@ -73,9 +73,9 @@ STATICFILES_DIRS = [
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+    # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
 ]
 
 
@@ -84,116 +84,111 @@ MIDDLEWARE = [
     # * SessionMiddleware (adds Cookie)
     # * GZipMiddleware (adds Accept-Encoding)
     # * LocaleMiddleware (adds Accept-Language)
-
     # Should go near top of the list:
-    'django.middleware.security.SecurityMiddleware',
+    "django.middleware.security.SecurityMiddleware",
     # Above all other middleware apart from Django's SecurityMiddleware:
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     # Must be before those that modify the `Vary` header:
-    'django.middleware.cache.UpdateCacheMiddleware',
+    "django.middleware.cache.UpdateCacheMiddleware",
     # Before any middleware that may change or use the response body:
-    'django.middleware.gzip.GZipMiddleware',
+    "django.middleware.gzip.GZipMiddleware",
     # After GZipMiddleware and close to top:
-    'django.middleware.common.CommonMiddleware',
+    "django.middleware.common.CommonMiddleware",
     # After UpdateCacheMiddleware:
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    "django.contrib.sessions.middleware.SessionMiddleware",
     # After SessionMiddleware:
-    'django.middleware.csrf.CsrfViewMiddleware',
+    "django.middleware.csrf.CsrfViewMiddleware",
     # After SessionMiddleware:
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
     # After SessionMiddleware:
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'pepysdiary.common.middleware.VisitTimeMiddleware',
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "pepysdiary.common.middleware.VisitTimeMiddleware",
     # Must be before those that modify the `Vary` header:
-    'django.middleware.cache.FetchFromCacheMiddleware',
+    "django.middleware.cache.FetchFromCacheMiddleware",
 ]
 
-CACHE_MIDDLEWARE_ALIAS = 'default'
+CACHE_MIDDLEWARE_ALIAS = "default"
 # Also see the CACHES setting in the server-specific settings files.
 CACHE_MIDDLEWARE_SECONDS = 500
-CACHE_MIDDLEWARE_KEY_PREFIX = 'pepys'
+CACHE_MIDDLEWARE_KEY_PREFIX = "pepys"
 
-ROOT_URLCONF = 'pepysdiary.common.urls'
+ROOT_URLCONF = "pepysdiary.common.urls"
 
 # Make this unique, and don't share it with anybody.
 # http://www.miniwebtool.com/django-secret-key-generator/
-SECRET_KEY = get_env_variable('SECRET_KEY')
+SECRET_KEY = get_env_variable("SECRET_KEY")
 
-ALLOWED_HOSTS = get_env_variable('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = get_env_variable("ALLOWED_HOSTS").split(",")
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'pepysdiary.wsgi.application'
+WSGI_APPLICATION = "pepysdiary.wsgi.application"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            'treebeard/templates/',
-            os.path.join(PROJECT_ROOT, 'templates', ),
-            os.path.join(PROJECT_ROOT, 'templates', 'common', ),
-            os.path.join(PROJECT_ROOT, 'templates', 'diary', ),
-            os.path.join(PROJECT_ROOT, 'templates', 'encyclopedia', ),
-            os.path.join(PROJECT_ROOT, 'templates', 'letters', ),
-            os.path.join(PROJECT_ROOT, 'templates', 'news', ),
-            os.path.join(PROJECT_ROOT, 'templates', 'membership', ),
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            "treebeard/templates/",
+            os.path.join(PROJECT_ROOT, "templates"),
+            os.path.join(PROJECT_ROOT, "templates", "common"),
+            os.path.join(PROJECT_ROOT, "templates", "diary"),
+            os.path.join(PROJECT_ROOT, "templates", "encyclopedia"),
+            os.path.join(PROJECT_ROOT, "templates", "letters"),
+            os.path.join(PROJECT_ROOT, "templates", "news"),
+            os.path.join(PROJECT_ROOT, "templates", "membership"),
         ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
                 # Needed for django-treebeard admin:
-                'django.template.context_processors.request',
-
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.media',
-                'pepysdiary.common.context_processors.api_keys',
-                'pepysdiary.common.context_processors.config',
-                'pepysdiary.common.context_processors.date_formats',
-                'pepysdiary.common.context_processors.url_name',
-            ],
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.media",
+                "pepysdiary.common.context_processors.api_keys",
+                "pepysdiary.common.context_processors.config",
+                "pepysdiary.common.context_processors.date_formats",
+                "pepysdiary.common.context_processors.url_name",
+            ]
         },
-    },
+    }
 ]
 
 
 INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.humanize',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.admin',
-    'django.contrib.sitemaps',
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.humanize",
+    "django.contrib.sessions",
+    "django.contrib.sites",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.admin",
+    "django.contrib.sitemaps",
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'django.contrib.flatpages',
+    "django.contrib.flatpages",
     # For fulltext searching on annotations:
     # 'django.contrib.postgres',
-    'treebeard',
-    'gunicorn',
-    'captcha',
-    'django_filters', # Used for rest_framework
-    'rest_framework',
-
+    "treebeard",
+    "gunicorn",
+    "captcha",
+    "django_filters",  # Used for rest_framework
+    "rest_framework",
     # Started breaking with Django 1.10.7 although it shouldn't:
     # 'memcache_status',
-
-    'pepysdiary.common',
-    'pepysdiary.api',
-    'pepysdiary.diary',
-    'pepysdiary.encyclopedia',
-    'pepysdiary.letters',
-    'pepysdiary.indepth',
-    'pepysdiary.news',
-    'pepysdiary.membership',
-    'pepysdiary.events',
-    'django_comments',
-    'pepysdiary.annotations',
+    "pepysdiary.common",
+    "pepysdiary.api",
+    "pepysdiary.diary",
+    "pepysdiary.encyclopedia",
+    "pepysdiary.letters",
+    "pepysdiary.indepth",
+    "pepysdiary.news",
+    "pepysdiary.membership",
+    "pepysdiary.events",
+    "django_comments",
+    "pepysdiary.annotations",
 ]
 
 # Password validation
@@ -201,35 +196,27 @@ INSTALLED_APPS = [
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
-AUTH_USER_MODEL = 'membership.Person'
+AUTH_USER_MODEL = "membership.Person"
 
-LOGIN_REDIRECT_URL = 'home'
-LOGIN_URL = 'login'
-LOGOUT_URL = 'logout'
+LOGIN_REDIRECT_URL = "home"
+LOGIN_URL = "login"
+LOGOUT_URL = "logout"
 
-COMMENTS_APP = 'pepysdiary.annotations'
+COMMENTS_APP = "pepysdiary.annotations"
 
-TEST_RUNNER = 'django.test.runner.DiscoverRunner'
-
+TEST_RUNNER = "django.test.runner.DiscoverRunner"
 
 
 ####################################################################
 # THIRD-PARTY APPS
 
-from datetime import date, timedelta
 future_date = date.today() + timedelta(days=365)
 
 # From https://github.com/praekelt/django-recaptcha
@@ -237,56 +224,49 @@ NOCAPTCHA = True
 
 # Storing Media files on AWS.
 
-DEFAULT_FILE_STORAGE = 'pepysdiary.common.s3utils.MediaS3Boto3Storage'
+DEFAULT_FILE_STORAGE = "pepysdiary.common.s3utils.MediaS3Boto3Storage"
 
-AWS_ACCESS_KEY_ID = get_env_variable('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = get_env_variable('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = get_env_variable('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = get_env_variable("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = get_env_variable("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = get_env_variable("AWS_STORAGE_BUCKET_NAME")
 
 AWS_QUERYSTRING_AUTH = False
 
 WS_DEFAULT_ACL = None
 
-S3_URL = 'https://%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+S3_URL = "https://%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
 # Store static and media files in separate directories:
 MEDIA_URL = S3_URL + MEDIA_URL
 
 
 REST_FRAMEWORK = {
     # e.g. for latitude/longitude:
-    'COERCE_DECIMAL_TO_STRING': False,
-    'DEFAULT_PAGINATION_CLASS': 'pepysdiary.api.pagination.CustomPagination',
-    'PAGE_SIZE': 50,
-
-    'DEFAULT_RENDERER_CLASSES': (
-        'pepysdiary.api.renderers.PrettyJSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
+    "COERCE_DECIMAL_TO_STRING": False,
+    "DEFAULT_PAGINATION_CLASS": "pepysdiary.api.pagination.CustomPagination",
+    "PAGE_SIZE": 50,
+    "DEFAULT_RENDERER_CLASSES": (
+        "pepysdiary.api.renderers.PrettyJSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
     ),
-    'DEFAULT_THROTTLE_CLASSES': (
-        'rest_framework.throttling.AnonRateThrottle',
-    ),
-    'DEFAULT_THROTTLE_RATES': {
-        'anon': '3660/hour', # 1 per second plus a bit.
-    },
-
-    'EXCEPTION_HANDLER': 'pepysdiary.api.views.custom_exception_handler',
+    "DEFAULT_THROTTLE_CLASSES": ("rest_framework.throttling.AnonRateThrottle",),
+    "DEFAULT_THROTTLE_RATES": {"anon": "3660/hour"},  # 1 per second plus a bit.
+    "EXCEPTION_HANDLER": "pepysdiary.api.views.custom_exception_handler",
 }
 
 # The REST_FRAMEWORK API documentation
 SWAGGER_SETTINGS = {
-    'DOC_EXPANSION': 'list',
-    'SUPPORTED_SUBMIT_METHODS': ['get',],
-    'USE_SESSION_AUTH': False,
-
+    "DOC_EXPANSION": "list",
+    "SUPPORTED_SUBMIT_METHODS": ["get"],
+    "USE_SESSION_AUTH": False,
 }
 
 ####################################################################
 # PEPYSDIARY SPECIFIC
 
 # For messages we send to users.
-DEFAULT_FROM_EMAIL = 'do-not-reply@pepysdiary.com'
+DEFAULT_FROM_EMAIL = "do-not-reply@pepysdiary.com"
 # For error messages.
-SERVER_EMAIL = 'do-not-reply@pepysdiary.com'
+SERVER_EMAIL = "do-not-reply@pepysdiary.com"
 
 # How many days do we give people to activate their account after registering?
 # If we run the cleanupactivation command, it will delete any dormant
