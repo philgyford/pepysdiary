7cdff297a2fa73a5820048a8b0acbddda331af99
Phil Gyford <phil@gyford.com>
2019-06-24 15:06:33 +0100

    Formatting

--- pepysdiary/common/templatetags/search_tags.py
+++ pepysdiary/common/templatetags/search_tags.py
@@ -13,21 +13,20 @@ def search_summary(obj, search_string):
 
     contents = []
 
-    if obj_name == 'Entry':
+    if obj_name == "Entry":
         contents = [obj.text, obj.footnotes]
-    elif obj_name == 'Topic':
+    elif obj_name == "Topic":
         contents = [obj.title, obj.summary_html, obj.wheatley_html]
-    elif obj_name == 'Annotation':
+    elif obj_name == "Annotation":
         contents = [obj.comment]
     else:
         # Letter, Article, Post
         contents = [obj.title, obj.intro_html, obj.text_html]
 
-    content = ' '.join(contents)
+    content = " ".join(contents)
 
     content = hilite_words(content, search_string)
 
     content = trim_hilites(content)
 
     return mark_safe(content)
-    
--- pepysdiary/common/utilities.py
+++ pepysdiary/common/utilities.py
@@ -6,12 +6,12 @@ from django.utils.feedgenerator import Rss201rev2Feed
 from django.utils.html import strip_tags
 
 
-def smart_truncate(content, length=80, suffix='…'):
+def smart_truncate(content, length=80, suffix="…"):
     """Truncate a string at word boundaries."""
     if len(content) <= length:
         return content
     else:
-        return content[:length].rsplit(' ', 1)[0] + suffix
+        return content[:length].rsplit(" ", 1)[0] + suffix
 
 
 def hilite_words(content, words):
@@ -25,12 +25,12 @@ def hilite_words(content, words):
     words -- Words to hilite. e.g. "cat dog" would result in any
              occurrences of "cat", "dog", "cats" or "dogs" being hilited.
              Ignores any punctuation characters in the words.
-    
+
     Returns a string.
     """
     # The HTML tags we'll wrap the hilited words in:
-    start_tag = '<b>'
-    end_tag = '</b>'
+    start_tag = "<b>"
+    end_tag = "</b>"
 
     # In case it's HTML.
     content = strip_tags(content)
@@ -40,30 +40,27 @@ def hilite_words(content, words):
     # the user has searched for <"my wife"> (including quotes), we'll have
     # results containing <my> and <wife> and <my wife>. And if we don't
     # strip non-word chars we're trying to hilite the strings <"my> and <wife">
-    regex = re.compile('[^\s0-9a-zA-Z]')
-    words = regex.sub(' ', words)
+    regex = re.compile(r"[^\s0-9a-zA-Z]")
+    words = regex.sub(" ", words)
 
     # If words = 'cat dog' then we look for '(\bcats?\b)|(\bdogs?\b)'
     # Note the optional trailing 's', because the search sometimes returns
     # matches for plurals, but we weren't hiliting them.
-    find = 's?\\b)|(\\b'.join(words.split())
-    regex = re.compile(r'(\b{}s?\b)'.format(find), re.IGNORECASE)
+    find = "s?\\b)|(\\b".join(words.split())
+    regex = re.compile(r"(\b{}s?\b)".format(find), re.IGNORECASE)
 
-    output = ''
+    output = ""
     i = 0
     for m in regex.finditer(content):
-        output += ''.join([
-                        content[i:m.start()],
-                        start_tag,
-                        content[m.start():m.end()],
-                        end_tag,
-                    ])
+        output += "".join(
+            [content[i: m.start()], start_tag, content[m.start(): m.end()], end_tag]
+        )
         i = m.end()
 
     if i == 0:
         html = content
     else:
-        html = ''.join([output, content[m.end():]])
+        html = "".join([output, content[m.end():]])
 
     return html
 
@@ -83,11 +80,11 @@ def trim_hilites(content, chars_before=20, chars_after=40):
 
     Returns a string.
     """
-    start_tag = '<b>'
-    end_tag = '</b>'
-    joiner = ' … '
+    start_tag = "<b>"
+    end_tag = "</b>"
+    joiner = " … "
 
-    regex = re.compile(r'{}[^<]*?{}'.format(start_tag, end_tag))
+    regex = re.compile(r"{}[^<]*?{}".format(start_tag, end_tag))
     iters = regex.finditer(content)
     # Get list of start and end positions of all '<b>blah</b>' substrings:
     positions = [(m.start(0), m.end(0)) for m in iters]
@@ -109,17 +106,20 @@ def trim_hilites(content, chars_before=20, chars_after=40):
             continue
         start = pos[0]
         end = pos[1]
-        for t in range(idx+1, len(positions)):
+        for t in range(idx + 1, len(positions)):
             # Go through all the subsequent positions, to see if they are
             # close enough to this one to keep them together.
             # What would be the cut-off point for the string sorrounding this position:
             trunc_point = end + chars_after
-            if (trunc_point >= positions[t][0] and trunc_point <= positions[t][1]) or (trunc_point >= positions[t][1]):
+            if (trunc_point >= positions[t][0] and trunc_point <= positions[t][1]) or (
+                trunc_point >= positions[t][1]
+            ):
                 # The cut-off point would be either within the next hilited
                 # word OR after the next hilited-word.
                 # So, we know we don't want to treat the next position separately:
                 positions_to_discard.append(positions[t])
-                # And we move the end point of this fragment ahead to include this next position:
+                # And we move the end point of this fragment ahead to
+                # include this next position:
                 end = positions[t][1]
             else:
                 # The next position isn't close to this one, so move on.
@@ -134,17 +134,17 @@ def trim_hilites(content, chars_before=20, chars_after=40):
     excerpts = []
     for pos in positions_to_use:
         if pos[0] > chars_before:
-            fragment = '{}'.format(content[pos[0]-chars_before:])
+            fragment = "{}".format(content[pos[0] - chars_before:])
         else:
             fragment = content
 
         # We have to allow for the length of the HTML tags:
         trunc_len = chars_before + (pos[1] - pos[0]) + chars_after
         truncated = fragment[:trunc_len]
-        if truncated != '':
+        if truncated != "":
             excerpts.append(truncated.strip())
 
-    text = joiner.join(excerpts) 
+    text = joiner.join(excerpts)
 
     if len(positions) > 0:
         if (positions[0][0] - chars_before) > 0:
@@ -154,10 +154,6 @@ def trim_hilites(content, chars_before=20, chars_after=40):
             text += joiner.rstrip()
 
     return text
-
-
-        
-
 
 
 # def hilite_words(content, words, chars_before=30, chars_after=50):
@@ -174,7 +170,7 @@ def trim_hilites(content, chars_before=20, chars_after=40):
 #     for word in words.split():
 #         find = r'({})'.format(re.escape(word))
 #         replace = r'<{}>\1</{}>'.format(tag, tag)
-#         hilite_re = re.compile(find, re.IGNORECASE) 
+#         hilite_re = re.compile(find, re.IGNORECASE)
 #         content = hilite_re.sub(replace, content)
 
 #     # Find all the matches, that we've now marked with <b></b>:
@@ -191,7 +187,9 @@ def trim_hilites(content, chars_before=20, chars_after=40):
 #             fragment = content
 
 #         # We have to allow for the length of the HTML tags:
-#         trunc_len = chars_before + (pos[1] - pos[0]) + chars_after - ((len(tag) * 2) + 5)
+#         trunc_len = chars_before + (
+#             pos[1] - pos[0]
+#         ) + chars_after - ((len(tag) * 2) + 5)
 #         truncated = Truncator(fragment).chars(
 #                                        trunc_len, truncate='', html=True)
 #         if truncated != '':
@@ -203,13 +201,16 @@ def trim_hilites(content, chars_before=20, chars_after=40):
 
 #     return text
 
+
 def is_leap_year(year):
     """Determine whether a year is a leap year."""
 
     return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 
 
-def expire_view_cache(view_name, args=[], namespace=None, key_prefix=None, method="GET"):
+def expire_view_cache(
+    view_name, args=[], namespace=None, key_prefix=None, method="GET"
+):
     """
     This function allows you to invalidate any view-level cache.
         view_name: view function you wish to invalidate or it's named url pattern
@@ -227,6 +228,7 @@ def expire_view_cache(view_name, args=[], namespace=None, key_prefix=None, metho
     from django.utils.cache import get_cache_key
     from django.core.cache import cache
     from django.conf import settings
+
     # create a fake request object
     request = HttpRequest()
     request.method = method
@@ -255,45 +257,57 @@ def fix_old_links(text):
 
     # From pepysdiary.com/p/42.php
     # to   pepysdiary.com/encyclopedia/42/
-    text = re.sub(r'pepysdiary\.com\/p\/(\d+)\.php',
-                    r'pepysdiary.com/encyclopedia/\1/',
-                    text)
+    text = re.sub(
+        r"pepysdiary\.com\/p\/(\d+)\.php", r"pepysdiary.com/encyclopedia/\1/", text
+    )
     # From pepysdiary.com/indepth/archive/2012/12/23/slug.php
     # to   pepysdiary.com/indepth/2012/12/23/slug/
-    text = re.sub(r'pepysdiary\.com\/indepth\/archive\/(.*?)\.php',
-                    r'pepysdiary.com/indepth/\1/',
-                    text)
+    text = re.sub(
+        r"pepysdiary\.com\/indepth\/archive\/(.*?)\.php",
+        r"pepysdiary.com/indepth/\1/",
+        text,
+    )
     # From pepysdiary.com/archive/1666/12/23/
     # or   pepysdiary.com/archive/1666/12/23/index.php
     # to   pepysdiary.com/diary/1666/12/23/
-    text = re.sub(r'pepysdiary\.com\/archive\/(\d\d\d\d\/\d\d\/\d\d)\/(index\.php)?',
-                    r'pepysdiary.com/diary/\1/',
-                    text)
+    text = re.sub(
+        r"pepysdiary\.com\/archive\/(\d\d\d\d\/\d\d\/\d\d)\/(index\.php)?",
+        r"pepysdiary.com/diary/\1/",
+        text,
+    )
     # From pepysdiary.com/letters/1666/12/23/pepys-to-evelyn.pyp
     # to   pepysdiary.com/letters/1666/12/23/pepys-to-evelyn/
-    text = re.sub(r'pepysdiary\.com\/letters\/(\d\d\d\d\/\d\d\/\d\d\/[\w-]+)\.php',
-                    r'pepysdiary.com/letters/\1/',
-                    text)
+    text = re.sub(
+        r"pepysdiary\.com\/letters\/(\d\d\d\d\/\d\d\/\d\d\/[\w-]+)\.php",
+        r"pepysdiary.com/letters/\1/",
+        text,
+    )
     # IMAGES
 
     # From pepysdiary.com/indepth/images/2012/05/31/SamuelPepys_1666.jpg
     # to   pepysdiary.com/static/img/indepth/2012/05/31/SamuelPepys_1666.jpg
-    text = re.sub(r'pepysdiary\.com\/indepth\/images\/(.*?\.(?:jpg|png|gif))',
-                    r'pepysdiary.com/static/img/indepth/\1',
-                    text)
+    text = re.sub(
+        r"pepysdiary\.com\/indepth\/images\/(.*?\.(?:jpg|png|gif))",
+        r"pepysdiary.com/static/img/indepth/\1",
+        text,
+    )
     # From pepysdiary.com/about/archive/files/2012/05/31/SamuelPepys_1666.jpg
     # to   pepysdiary.com/static/img/news/2012/05/31/SamuelPepys_1666.jpg
-    text = re.sub(r'pepysdiary\.com\/about\/archive\/files\/(.*?\.(?:jpg|png|gif))',
-                    r'pepysdiary.com/static/img/news/\1',
-                    text)
+    text = re.sub(
+        r"pepysdiary\.com\/about\/archive\/files\/(.*?\.(?:jpg|png|gif))",
+        r"pepysdiary.com/static/img/news/\1",
+        text,
+    )
 
     # OTHER FILES
 
     # From pepysdiary.com/about/archive/files/2009/03/23/ParallelLivesFlyer2009.pdf
     # to   pepysdiary.com/static/files/news/2009/03/23/ParallelLivesFlyer2009.pdf
-    text = re.sub(r'pepysdiary\.com\/about\/archive\/files\/(.*?\.(?:mp3|pdf|doc|docx|zip|html))',
-                    r'pepysdiary.com/static/files/news/\1',
-                    text)
+    text = re.sub(
+        r"pepysdiary\.com\/about\/archive\/files\/(.*?\.(?:mp3|pdf|doc|docx|zip|html))",
+        r"pepysdiary.com/static/files/news/\1",
+        text,
+    )
     return text
 
 
@@ -303,7 +317,7 @@ def get_year(date_obj):
     Because strftime can't cope with very old dates.
     `date` is a date object.
     """
-    return date_obj.isoformat().split('T')[0].split('-')[0]
+    return date_obj.isoformat().split("T")[0].split("-")[0]
 
 
 def get_month(date_obj):
@@ -311,7 +325,7 @@ def get_month(date_obj):
     Return month from the date like '01', '02', '12', etc.
     Because strftime can't cope with very old dates.
     """
-    return date_obj.isoformat().split('T')[0].split('-')[1]
+    return date_obj.isoformat().split("T")[0].split("-")[1]
 
 
 def get_month_b(date_obj):
@@ -319,9 +333,20 @@ def get_month_b(date_obj):
     Return month from the date like 'Jan', 'Feb', 'Dec', etc.
     Because strftime can't cope with very old dates.
     """
-    months = {'01': 'Jan', '02': 'Feb', '03': 'Mar', '04': 'Apr',
-                '05': 'May', '06': 'Jun', '07': 'Jul', '08': 'Aug',
-                '09': 'Sep', '10': 'Oct', '11': 'Nov', '12': 'Dec', }
+    months = {
+        "01": "Jan",
+        "02": "Feb",
+        "03": "Mar",
+        "04": "Apr",
+        "05": "May",
+        "06": "Jun",
+        "07": "Jul",
+        "08": "Aug",
+        "09": "Sep",
+        "10": "Oct",
+        "11": "Nov",
+        "12": "Dec",
+    }
     return months[get_month(date_obj)]
 
 
@@ -330,7 +355,7 @@ def get_day(date_obj):
     Return day from the date like '01', '02', '31', etc.
     Because strftime can't cope with very old dates.
     """
-    return date_obj.isoformat().split('T')[0].split('-')[2]
+    return date_obj.isoformat().split("T")[0].split("-")[2]
 
 
 def get_day_e(date_obj):
@@ -339,7 +364,7 @@ def get_day_e(date_obj):
     Because strftime can't cope with very old dates.
     """
     d = get_day(date_obj)
-    if d[:1] == '0':
+    if d[:1] == "0":
         return d[1:]
     else:
         return d
@@ -350,15 +375,15 @@ def make_url_absolute(url):
     Pass a url like '/encyclopedia/123/' and it will return a url more like
     'https://yourdomain.com/encyclopedia/123/'
     """
-    if url.startswith('http'):
+    if url.startswith("http"):
         # It's already absolute!
         return url
     else:
-        protocol = 'https' if settings.SECURE_SSL_REDIRECT else 'http'
+        protocol = "https" if settings.SECURE_SSL_REDIRECT else "http"
 
         domain = Site.objects.get_current().domain
 
-        return '{}://{}{}'.format(protocol, domain, url)
+        return "{}://{}{}".format(protocol, domain, url)
 
 
 class ExtendedRSSFeed(Rss201rev2Feed):
@@ -366,11 +391,12 @@ class ExtendedRSSFeed(Rss201rev2Feed):
     Create a type of RSS feed that has content:encoded elements.
     Should be used as the feed_type for View classes that inherit Feed.
     """
+
     def root_attributes(self):
         attrs = super(ExtendedRSSFeed, self).root_attributes()
-        attrs['xmlns:content'] = 'http://purl.org/rss/1.0/modules/content/'
+        attrs["xmlns:content"] = "http://purl.org/rss/1.0/modules/content/"
         return attrs
 
     def add_item_elements(self, handler, item):
         super(ExtendedRSSFeed, self).add_item_elements(handler, item)
-        handler.addQuickElement('content:encoded', item['content_encoded'])
+        handler.addQuickElement("content:encoded", item["content_encoded"])
