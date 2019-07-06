05ef39c595850a52f0c298c0d498e6719cb3ad80
Phil Gyford <phil@gyford.com>
2019-07-06 21:53:35 +0100

    Fix bug with search index on Django 2.2.1
    After upgrading to Django 2.2 I got an error when posting a new
    annotation:
    
    ```
    django.core.exceptions.FieldError: Cannot resolve expression type, unknown output_field
    ```
    
    Which was happening with this in
    `pepysdiary.common.signals.make_updater()`:
    
    ```
    search_vectors.append(
        SearchVector(Value(text, output_field=TextField()), weight=weight)
    )
    ```
    
    Googling, it seems to be [a bug in Django 2.2](https://code.djangoproject.com/ticket/30446)
    
    Fixed it as suggested there, and the same way that Simon Willison
    did in https://github.com/simonw/simonwillisonblog/pull/25

--- pepysdiary/common/signals.py
+++ pepysdiary/common/signals.py
@@ -3,7 +3,7 @@ import operator
 
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from django.db.models import Value
+from django.db.models import Value, TextField
 from django.contrib.postgres.search import SearchVector
 from django.db import transaction
 
@@ -19,10 +19,14 @@ def on_save(sender, **kwargs):
     """Only do something if this object has a search_document property
     and an index_components() method.
     """
-    obj = kwargs['instance']
-    if not hasattr(obj, 'search_document') or not hasattr(obj, 'index_components') or not callable(obj.index_components):
+    obj = kwargs["instance"]
+    if (
+        not hasattr(obj, "search_document")
+        or not hasattr(obj, "index_components")
+        or not callable(obj.index_components)
+    ):
         return
-    transaction.on_commit(make_updater(kwargs['instance']))
+    transaction.on_commit(make_updater(kwargs["instance"]))
 
 
 def make_updater(instance):
@@ -43,9 +47,10 @@ def make_updater(instance):
         search_vectors = []
         for weight, text in components.items():
             search_vectors.append(
-                SearchVector(Value(text), weight=weight)
+                SearchVector(Value(text, output_field=TextField()), weight=weight)
             )
         instance.__class__.objects.filter(pk=pk).update(
             search_document=reduce(operator.add, search_vectors)
         )
+
     return on_commit
