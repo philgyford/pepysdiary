b15e7f30851ef289df8d561b821f768f7e6d08be
Phil Gyford <phil@gyford.com>
2020-02-08 13:50:20 +0000

    Formatting

--- pepysdiary/encyclopedia/tests/test_management_commands.py
+++ pepysdiary/encyclopedia/tests/test_management_commands.py
@@ -1,4 +1,3 @@
-#! -*- coding: utf-8 -*-
 from unittest.mock import patch
 
 from django.core.management import call_command
@@ -16,60 +15,64 @@ class FetchWikipediaTest(PepysdiaryTestCase):
 
     def test_no_args(self):
         with self.assertRaises(CommandError):
-            call_command('fetch_wikipedia')
+            call_command("fetch_wikipedia")
 
     def test_wrong_args(self):
         with self.assertRaises(CommandError):
-            call_command('fetch_wikipedia', 'all')
+            call_command("fetch_wikipedia", "all")
 
-    @patch('pepysdiary.encyclopedia.models.TopicManager.fetch_wikipedia_texts')
+    @patch("pepysdiary.encyclopedia.models.TopicManager.fetch_wikipedia_texts")
     def test_with_single_topic_id(self, fetch_method):
-        call_command('fetch_wikipedia', ids=[112], stdout=StringIO())
+        call_command("fetch_wikipedia", ids=[112], stdout=StringIO())
         fetch_method.assert_called_with(topic_ids=[112])
 
-    @patch('pepysdiary.encyclopedia.models.TopicManager.fetch_wikipedia_texts')
+    @patch("pepysdiary.encyclopedia.models.TopicManager.fetch_wikipedia_texts")
     def test_with_multiple_topic_ids(self, fetch_method):
-        call_command('fetch_wikipedia', ids=[112, 344, 6079], stdout=StringIO())
+        call_command("fetch_wikipedia", ids=[112, 344, 6079], stdout=StringIO())
         fetch_method.assert_called_with(topic_ids=[112, 344, 6079])
 
-    @patch('pepysdiary.encyclopedia.models.TopicManager.fetch_wikipedia_texts')
+    @patch("pepysdiary.encyclopedia.models.TopicManager.fetch_wikipedia_texts")
     def test_with_all(self, fetch_method):
-        call_command('fetch_wikipedia', all=True, stdout=StringIO())
-        fetch_method.assert_called_with(num='all')
+        call_command("fetch_wikipedia", all=True, stdout=StringIO())
+        fetch_method.assert_called_with(num="all")
 
-    @patch('pepysdiary.encyclopedia.models.TopicManager.fetch_wikipedia_texts')
+    @patch("pepysdiary.encyclopedia.models.TopicManager.fetch_wikipedia_texts")
     def test_with_num(self, fetch_method):
-        call_command('fetch_wikipedia', num=30, stdout=StringIO())
+        call_command("fetch_wikipedia", num=30, stdout=StringIO())
         fetch_method.assert_called_with(num=30)
 
-    @patch('pepysdiary.encyclopedia.models.TopicManager.fetch_wikipedia_texts')
+    @patch("pepysdiary.encyclopedia.models.TopicManager.fetch_wikipedia_texts")
     def test_with_default_verbosity(self, fetch_method):
-        # What the mocked method will return:
-        fetch_method.side_effect = [{'success': [112], 'failure': []}]
+        #  What the mocked method will return:
+        fetch_method.side_effect = [{"success": [112], "failure": []}]
         out = StringIO()
-        call_command('fetch_wikipedia', ids=[112], stdout=out)
-        self.assertIn('Successfully fetched 1 topic(s)', out.getvalue())
+        call_command("fetch_wikipedia", ids=[112], stdout=out)
+        self.assertIn("Successfully fetched 1 topic(s)", out.getvalue())
 
-    @patch('pepysdiary.encyclopedia.models.TopicManager.fetch_wikipedia_texts')
+    @patch("pepysdiary.encyclopedia.models.TopicManager.fetch_wikipedia_texts")
     def test_with_zero_verbosity(self, fetch_method):
-        # What the mocked method will return:
-        fetch_method.side_effect = [{'success': [112], 'failure': []}]
+        #  What the mocked method will return:
+        fetch_method.side_effect = [{"success": [112], "failure": []}]
         out = StringIO()
-        call_command('fetch_wikipedia', ids=[112], verbosity=0, stdout=out)
-        self.assertNotIn('Successfully fetched 1 topic(s)', out.getvalue())
+        call_command("fetch_wikipedia", ids=[112], verbosity=0, stdout=out)
+        self.assertNotIn("Successfully fetched 1 topic(s)", out.getvalue())
 
-    @patch('pepysdiary.encyclopedia.models.TopicManager.fetch_wikipedia_texts')
+    @patch("pepysdiary.encyclopedia.models.TopicManager.fetch_wikipedia_texts")
     def test_with_extra_verbosity(self, fetch_method):
-        # What the mocked method will return:
-        fetch_method.side_effect = [{'success': [112, 150], 'failure': [344]}]
+        #  What the mocked method will return:
+        fetch_method.side_effect = [{"success": [112, 150], "failure": [344]}]
         out = StringIO()
         out_err = StringIO()
-        call_command('fetch_wikipedia', ids=[112, 344, 150], verbosity=2,
-                                                    stdout=out, stderr=out_err)
-        self.assertIn('Successfully fetched 2 topic(s)', out.getvalue())
-        self.assertIn('IDs: 112, 150', out.getvalue())
-        self.assertIn('Tried and failed to fetch texts for 1 topic(s)',
-                                                            out_err.getvalue())
-        self.assertIn('IDs: 344', out_err.getvalue())
-
-
+        call_command(
+            "fetch_wikipedia",
+            ids=[112, 344, 150],
+            verbosity=2,
+            stdout=out,
+            stderr=out_err,
+        )
+        self.assertIn("Successfully fetched 2 topic(s)", out.getvalue())
+        self.assertIn("IDs: 112, 150", out.getvalue())
+        self.assertIn(
+            "Tried and failed to fetch texts for 1 topic(s)", out_err.getvalue()
+        )
+        self.assertIn("IDs: 344", out_err.getvalue())
--- pepysdiary/encyclopedia/tests/test_wikipedia_fetcher.py
+++ pepysdiary/encyclopedia/tests/test_wikipedia_fetcher.py
@@ -9,55 +9,55 @@ from pepysdiary.encyclopedia.wikipedia_fetcher import WikipediaFetcher
 
 class FetchTest(PepysdiaryTestCase):
 
-    source_html = '<p>Hello</p>'
-    page_name = 'Edward_Montagu%2C_1st_Earl_of_Sandwich'
+    source_html = "<p>Hello</p>"
+    page_name = "Edward_Montagu%2C_1st_Earl_of_Sandwich"
 
     def add_response(self, body, status=200):
         responses.add(
             responses.GET,
-            'https://en.wikipedia.org/wiki/%s?action=render' % self.page_name,
+            "https://en.wikipedia.org/wiki/%s?action=render" % self.page_name,
             match_querystring=True,
             status=status,
-            body=body
+            body=body,
         )
 
     @responses.activate
     def test_it_sends_a_request(self):
         self.add_response(body=self.source_html)
         result = WikipediaFetcher()._get_html(self.page_name)
-        self.assertTrue(result['success'])
-        self.assertEqual(result['content'], self.source_html)
+        self.assertTrue(result["success"])
+        self.assertEqual(result["content"], self.source_html)
 
     @responses.activate
     def test_it_handles_exceptions(self):
         errors = (
-            (ConnectionError,   "Can't connect to domain."),
-            (Timeout,           "Connection timed out."),
-            (TooManyRedirects,  "Too many redirects."),
+            (ConnectionError, "Can't connect to domain."),
+            (Timeout, "Connection timed out."),
+            (TooManyRedirects, "Too many redirects."),
         )
         for error, message in errors:
             self.add_response(body=error())
             result = WikipediaFetcher()._get_html(self.page_name)
-            self.assertFalse(result['success'])
-            self.assertEqual(result['content'], message)
+            self.assertFalse(result["success"])
+            self.assertEqual(result["content"], message)
             responses.reset()
 
     @responses.activate
     def test_it_handles_404s(self):
-        self.add_response(body='<h1>Not found</h1>', status=404)
+        self.add_response(body="<h1>Not found</h1>", status=404)
         result = WikipediaFetcher()._get_html(self.page_name)
-        self.assertFalse(result['success'])
-        self.assertEqual(result['content'], 'HTTP Error: 404')
+        self.assertFalse(result["success"])
+        self.assertEqual(result["content"], "HTTP Error: 404")
 
     @responses.activate
     def test_it_handles_500s(self):
-        self.add_response(body='<h1>Not found</h1>', status=500)
+        self.add_response(body="<h1>Not found</h1>", status=500)
         result = WikipediaFetcher()._get_html(self.page_name)
-        self.assertFalse(result['success'])
-        self.assertEqual(result['content'], 'HTTP Error: 500')
+        self.assertFalse(result["success"])
+        self.assertEqual(result["content"], "HTTP Error: 500")
 
     @responses.activate
-    @patch('pepysdiary.encyclopedia.wikipedia_fetcher.WikipediaFetcher._tidy_html')
+    @patch("pepysdiary.encyclopedia.wikipedia_fetcher.WikipediaFetcher._tidy_html")
     def test_it_filters_returned_html(self, filter_method):
         """
         Test that the fetch() method will pass the results of _get_html() to
@@ -72,51 +72,58 @@ class FetchTest(PepysdiaryTestCase):
         # Check _tidy_html() was called with what _get_html() returned:
         filter_method.assert_called_with(self.source_html)
         # Whatever called fetch() should get this back:
-        self.assertEqual(result,
-                         {'success': True, 'content': self.source_html})
+        self.assertEqual(result, {"success": True, "content": self.source_html})
 
     def test_it_removes_disallowed_tags(self):
-        in_html = '<blink>Blinking</blink> <strong>Bold</strong>'
-        out_html = 'Blinking <strong>Bold</strong>'
+        in_html = "<blink>Blinking</blink> <strong>Bold</strong>"
+        out_html = "Blinking <strong>Bold</strong>"
         self.assertEqual(WikipediaFetcher()._tidy_html(in_html), out_html)
 
     def test_it_removes_disallowed_attributes(self):
-        in_html = ('<a class="my-class" href="test.html" '
-                   'data-bad="My data">Link</a>')
+        in_html = '<a class="my-class" href="test.html" ' 'data-bad="My data">Link</a>'
         out_html = '<a class="my-class" href="test.html">Link</a>'
         self.assertEqual(WikipediaFetcher()._tidy_html(in_html), out_html)
 
     def test_it_removes_disallowed_selectors(self):
-        in_html = ('<div>This is OK</div>'
-                   '<div class="navbar mini">This is not</div>'
-                   '<div>This is also fine</div>'
-                   '<div class="mini navbar">This is not either</div>')
-        out_html = '<div>This is OK</div><div>This is also fine</div>'
+        in_html = (
+            "<div>This is OK</div>"
+            '<div class="navbar mini">This is not</div>'
+            "<div>This is also fine</div>"
+            '<div class="mini navbar">This is not either</div>'
+        )
+        out_html = "<div>This is OK</div><div>This is also fine</div>"
         self.assertEqual(WikipediaFetcher()._strip_html(in_html), out_html)
 
     def test_it_removes_disallowed_classes(self):
-        in_html = ('<div>This is OK</div><div class="noprint">This is not'
-                   '</div><div>This is also fine</div><p class="noprint">This '
-                   'is not either</p>')
-        out_html = '<div>This is OK</div><div>This is also fine</div>'
+        in_html = (
+            '<div>This is OK</div><div class="noprint">This is not'
+            '</div><div>This is also fine</div><p class="noprint">This '
+            "is not either</p>"
+        )
+        out_html = "<div>This is OK</div><div>This is also fine</div>"
         self.assertEqual(WikipediaFetcher()._strip_html(in_html), out_html)
 
     def test_it_adds_classes(self):
-        in_html = ('<div class="infobox">Infobox</div>'
-                   '<div class="bibble">Untouched</div>'
-                   '<div class="infobox">Infobox 2</div>')
-        out_html = ('<div class="infobox table table-bordered">Infobox</div>'
-                    '<div class="bibble">Untouched</div>'
-                    '<div class="infobox table table-bordered">Infobox 2'
-                    '</div>')
+        in_html = (
+            '<div class="infobox">Infobox</div>'
+            '<div class="bibble">Untouched</div>'
+            '<div class="infobox">Infobox 2</div>'
+        )
+        out_html = (
+            '<div class="infobox table table-bordered">Infobox</div>'
+            '<div class="bibble">Untouched</div>'
+            '<div class="infobox table table-bordered">Infobox 2'
+            "</div>"
+        )
         self.assertEqual(WikipediaFetcher()._strip_html(in_html), out_html)
 
     def test_it_removes_scripts(self):
         "All <script> tags and contents should be removed."
-        in_html = ('<div class="bibble">'
-                   '<script>console.log("This should be removed.")</script>'
-                   'This should show up.'
-                   '</div>')
-        out_html = ('<div class="bibble">This should show up.</div>')
+        in_html = (
+            '<div class="bibble">'
+            '<script>console.log("This should be removed.")</script>'
+            "This should show up."
+            "</div>"
+        )
+        out_html = '<div class="bibble">This should show up.</div>'
         self.assertEqual(WikipediaFetcher()._strip_html(in_html), out_html)
-
