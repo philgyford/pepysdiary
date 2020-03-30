e5dabcdd3f6f10d15e16bc5d3e8486bbd39603e7
Phil Gyford <phil@gyford.com>
2020-03-30 12:39:03 +0100

    Better handle common words with leading apostrophes
    Smartypants was doing their curly quotes the wrong way
    
    Fixes #152

--- pepysdiary/common/templatetags/text_formatting_filters.py
+++ pepysdiary/common/templatetags/text_formatting_filters.py
@@ -1,4 +1,4 @@
-import re   
+import re
 from django import template
 from django.utils.safestring import mark_safe
 
@@ -7,19 +7,41 @@ register = template.Library()
 
 @register.filter
 def smartypants(value):
+    """
+    Modified of smartypants
+
+    Handles some exceptions that don't use standard curly quotes.
+
+    e.g. 'Change should not be ‘Change but ’Change.
+    i.e. some words use a curly apostophe the other way.
+
+    """
     try:
         import smartypants
+
+        replacements = (
+            ("'Change", "&#8217;Change"),
+            ("'Chequer", "&#8217;Chequer"),
+            ("'light", "&#8217;light"),
+            ("'prentice", "&#8217;prentice"),
+            ("'prentices", "&#8217;prentices"),
+        )
+        for rep in replacements:
+            value = re.sub(rf"(\W){rep[0]}(\W)", rf"\1{rep[1]}\2", value)
+
         return smartypants.smartypants(value)
     except ImportError:
         return value
 
+
 @register.filter()
 def markup_tooltip(value):
-    if re.match('\d{4}-\d{4}', value) is not None:
-        value = re.sub(r'^(\d{4})-(\d{4})',
-                    r'<span itemprop="birthDate">\1</span>-<span itemprop="deathDate">\2</span>',
-                    value)
+    if re.match(r"\d{4}-\d{4}", value) is not None:
+        value = re.sub(
+            r"^(\d{4})-(\d{4})",
+            r'<span itemprop="birthDate">\1</span>-<span itemprop="deathDate">\2</span>',  # noqa: E501
+            value,
+        )
         value = mark_safe(value)
 
     return value
-
--- pepysdiary/common/tests/test_templatetags.py
+++ pepysdiary/common/tests/test_templatetags.py
@@ -1,7 +1,29 @@
 # coding: utf-8
 from pepysdiary.common.tests.test_base import PepysdiaryTestCase
 
-from pepysdiary.common.templatetags.text_formatting_filters import markup_tooltip
+from pepysdiary.common.templatetags.text_formatting_filters import (
+    markup_tooltip,
+    smartypants,
+)
+
+
+class SmartypantsTestCase(PepysdiaryTestCase):
+    def test_standard(self):
+        val = smartypants("""'This is -- "a test"'""")
+        self.assertEqual(val, "&#8216;This is &#8212; &#8220;a test&#8221;&#8217;")
+
+    def test_special_cases(self):
+        "Our special cases should be replaced"
+        cases = (
+            ("the 'Change was", "the &#8217;Change was"),
+            ("the 'Chequer,", "the &#8217;Chequer,"),
+            ("a 'light which", "a &#8217;light which"),
+            ("a 'prentice was", "a &#8217;prentice was"),
+            ("the 'prentices were", "the &#8217;prentices were"),
+        )
+
+        for case in cases:
+            self.assertEqual(smartypants(case[0]), case[1])
 
 
 class MarkupTooltipTestCase(PepysdiaryTestCase):
@@ -13,5 +35,6 @@ class MarkupTooltipTestCase(PepysdiaryTestCase):
         val = markup_tooltip("1601-1689. Hello there.")
         self.assertEqual(
             val,
-            '<span itemprop="birthDate">1601</span>-<span itemprop="deathDate">1689</span>. Hello there.',
+            '<span itemprop="birthDate">1601</span>'
+            '-<span itemprop="deathDate">1689</span>. Hello there.',
         )
