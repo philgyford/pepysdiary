ae984f1cbb8287853cd0e7b80ec3dc444ac63c24
Phil Gyford <phil@gyford.com>
2020-03-31 17:59:55 +0100

    Formatting

--- pepysdiary/indepth/admin.py
+++ pepysdiary/indepth/admin.py
@@ -4,21 +4,48 @@ from pepysdiary.indepth.models import Article
 
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'status', 'date_published', 'comment_count', )
-    search_fields = ['title', 'intro', 'text', ]
-    readonly_fields = ('date_created', 'date_modified', 'last_comment_time', )
-    fieldsets = (
-        (None, {
-            'fields': ('title', 'slug', 'status', 'date_published',
-                                                            'allow_comments', )
-        }),
-        (None, {
-            'fields': ('intro', 'text', 'excerpt', )
-        }),
-        (None, {
-            'fields': ('date_created', 'date_modified', 'comment_count',
-                                                        'last_comment_time', ),
-        }),
+    list_display = (
+        "title",
+        "status",
+        "date_published",
+        "comment_count",
     )
+    search_fields = [
+        "title",
+        "intro",
+        "text",
+    ]
+    readonly_fields = (
+        "date_created",
+        "date_modified",
+        "last_comment_time",
+    )
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "title",
+                    "slug",
+                    "status",
+                    "date_published",
+                    "allow_comments",
+                )
+            },
+        ),
+        (None, {"fields": ("intro", "text", "excerpt",)}),
+        (
+            None,
+            {
+                "fields": (
+                    "date_created",
+                    "date_modified",
+                    "comment_count",
+                    "last_comment_time",
+                ),
+            },
+        ),
+    )
+
 
 admin.site.register(Article, ArticleAdmin)
