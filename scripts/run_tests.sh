e3d3efef693e996663ec22859a91d5a43c02a289
Phil Gyford <phil@gyford.com>
2021-04-15 18:29:31 +0100

    Fix test script; wasn't running flake8 and coverage

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -9,10 +9,9 @@ set -e
 # e.g.
 # ./run_tests.sh tests.appname.test_models.TestClass.test_a_thing
 # In this case Flake8 and Coverage will NOT be run.
-TESTS_TO_RUN=${1:tests}
+TESTS_TO_RUN=${1:-tests}
 
-if [[ $TESTS_TO_RUN == "tests" ]]
-then
+if [[ "$TESTS_TO_RUN" == "tests" ]]; then
     # Coverage config is in setup.cfg
     docker exec pepys_web /bin/sh -c "pipenv run coverage run manage.py test --settings=pepysdiary.settings.tests $TESTS_TO_RUN ; pipenv run flake8; pipenv run coverage html"
 else
