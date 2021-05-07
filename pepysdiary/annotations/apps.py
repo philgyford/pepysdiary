8f382288c789cdf99be2bb36655393515dec3902
Phil Gyford <phil@gyford.com>
2021-05-07 15:12:09 +0100

    Add pykismet for spam checking, for consistency with django-hines

--- pepysdiary/annotations/apps.py
+++ pepysdiary/annotations/apps.py
@@ -7,3 +7,4 @@ class AnnotationsConfig(AppConfig):
 
     def ready(self):
         from pepysdiary.common import signals  # noqa: F401
+        from . import signals as annotation_signals  # noqa: F401
