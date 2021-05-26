9ac19e21569b602d404cd7831654dea0a927c839
Phil Gyford <phil@gyford.com>
2021-05-26 11:17:36 +0100

    Remove unused validation from map form
    Having the `choices` on the category ID input already enforces that
    the submitted category ID is valid, by Django's built in validation,
    so no need to do it again.

--- pepysdiary/encyclopedia/forms.py
+++ pepysdiary/encyclopedia/forms.py
@@ -14,9 +14,3 @@ class CategoryMapForm(forms.Form):
         widget=forms.Select(attrs={"class": "form-control"}),
         choices=Category.objects.map_category_choices(),
     )
-
-    def clean_category(self):
-        category = self.cleaned_data["category"]
-        if int(category) not in Category.objects.valid_map_category_ids():
-            raise forms.ValidationError("'%s' is not a valid category ID" % category)
-        return category
