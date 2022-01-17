edd7f61aeeb9e275957b5a41813234ae3bab0074
Phil Gyford <phil@gyford.com>
2022-01-17 10:54:44 +0000

    Replace ugettext with gettext for Django 4

--- pepysdiary/encyclopedia/views.py
+++ pepysdiary/encyclopedia/views.py
@@ -3,7 +3,7 @@ import string
 from django.urls import reverse
 from django.http import Http404, HttpResponseRedirect
 from django.utils.decorators import method_decorator
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext as _
 from django.views.decorators.csrf import csrf_protect
 from django.views.generic import FormView, TemplateView
 from django.views.generic.detail import DetailView
