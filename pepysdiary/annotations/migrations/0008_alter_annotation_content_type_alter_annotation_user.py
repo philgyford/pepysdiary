006121a8c334e63fdf1346348fd9b0b21cfe2e4f
Phil Gyford <phil@gyford.com>
2022-01-20 15:39:47 +0000

    Formatting

--- pepysdiary/annotations/migrations/0008_alter_annotation_content_type_alter_annotation_user.py
+++ pepysdiary/annotations/migrations/0008_alter_annotation_content_type_alter_annotation_user.py
@@ -9,19 +9,31 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('contenttypes', '0002_remove_content_type_name'),
-        ('annotations', '0007_auto_20210603_1459'),
+        ("contenttypes", "0002_remove_content_type_name"),
+        ("annotations", "0007_auto_20210603_1459"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='annotation',
-            name='content_type',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='content_type_set_for_%(class)s', to='contenttypes.contenttype', verbose_name='content type'),
+            model_name="annotation",
+            name="content_type",
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="content_type_set_for_%(class)s",
+                to="contenttypes.contenttype",
+                verbose_name="content type",
+            ),
         ),
         migrations.AlterField(
-            model_name='annotation',
-            name='user',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='%(class)s_comments', to=settings.AUTH_USER_MODEL, verbose_name='user'),
+            model_name="annotation",
+            name="user",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                related_name="%(class)s_comments",
+                to=settings.AUTH_USER_MODEL,
+                verbose_name="user",
+            ),
         ),
     ]
