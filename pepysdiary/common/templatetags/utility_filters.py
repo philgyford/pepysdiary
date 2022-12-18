fa98cbba3f3d9ad6485aefb335818eb999aa2245
Phil Gyford <phil@gyford.com>
2022-12-18 14:34:28 +0000

    Use words to mark readings in comments, instead of numbers
    So, "First Reading" instead of "Reading 1"
    
    For #26

--- pepysdiary/common/templatetags/utility_filters.py
+++ pepysdiary/common/templatetags/utility_filters.py
@@ -1,4 +1,5 @@
 from django import template
+from django.contrib.humanize.templatetags.humanize import ordinal
 
 register = template.Library()
 
@@ -10,3 +11,24 @@ def to_class_name(value):
     {{ obj|to_class_name }}
     """
     return value.__class__.__name__
+
+
+@register.filter
+def ordinal_word(value):
+    terms = {
+        1: "first",
+        2: "second",
+        3: "third",
+        4: "fourth",
+        5: "fifth",
+        6: "sixth",
+        7: "seventh",
+        8: "eighth",
+        9: "ningth",
+        10: "tenth",
+    }
+
+    if value in terms:
+        return terms[value]
+    else:
+        return ordinal(value)
--- tests/common/templatetags/test_utility_filters.py
+++ tests/common/templatetags/test_utility_filters.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from pepysdiary.common.templatetags.utility_filters import to_class_name
+from pepysdiary.common.templatetags.utility_filters import ordinal_word, to_class_name
 from pepysdiary.diary.factories import EntryFactory
 from pepysdiary.encyclopedia.factories import TopicFactory
 from pepysdiary.letters.factories import LetterFactory
@@ -15,3 +15,28 @@ class ToClassNameTestCase(TestCase):
 
     def test_topic(self):
         self.assertEqual(to_class_name(TopicFactory()), "Topic")
+
+
+class OrdinalWordTestCase(TestCase):
+    def test_numbers(self):
+        terms = {
+            # Terms set in the function:
+            1: "first",
+            2: "second",
+            3: "third",
+            4: "fourth",
+            5: "fifth",
+            6: "sixth",
+            7: "seventh",
+            8: "eighth",
+            9: "ningth",
+            10: "tenth",
+            # Terms that come from the standard ordinal template filter:
+            11: "11th",
+            20: "20th",
+            21: "21st",
+            1234: "1234th",
+        }
+
+        for num, term in terms.items():
+            self.assertEqual(ordinal_word(num), term)
