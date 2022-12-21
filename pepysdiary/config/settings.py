4dd13199042d5f9f24ec71e0bc7a276efa2d78c9
Phil Gyford <phil@gyford.com>
2022-12-21 14:46:21 +0000

    Tweak Sentry settings

--- pepysdiary/config/settings.py
+++ pepysdiary/config/settings.py
@@ -354,6 +354,10 @@ if SENTRY_DSN:
     sentry_sdk.init(
         dsn=SENTRY_DSN,
         integrations=[DjangoIntegration()],
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=0.1,
     )
 
 # END THIRD-PARTY APP SETTINGS
