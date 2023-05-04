cb4ecc87a933d3cc27efd059426d79c58221ce57
Phil Gyford <phil@gyford.com>
2023-05-04 15:28:45 +0100

    Update python dependencies, including Django from 4.1 to 4.2.1
    And psycopg2 to psycopg3, and update staticfiles settings to the
    Django 4.2 way of doing things.

--- pepysdiary/config/settings.py
+++ pepysdiary/config/settings.py
@@ -147,7 +147,15 @@ USE_I18N = False
 USE_TZ = True
 
 
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
+
 
 STATIC_ROOT = BASE_DIR / "pepysdiary" / "static_collected"
 
@@ -169,7 +177,7 @@ MEDIA_URL = "/media/"
 
 if os.getenv("PEPYS_USE_AWS_FOR_MEDIA", default="False") == "True":
     # Storing Media files on AWS.
-    DEFAULT_FILE_STORAGE = "pepysdiary.common.storage.MediaS3Boto3Storage"
+    STORAGES["default"]["BACKEND"] = "pepysdiary.common.storage.MediaS3Boto3Storage"
 
     AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
