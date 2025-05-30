c0b5faf700de24a10de84afe2b0bf2a83822ff10
Phil Gyford <phil@gyford.com>
2025-05-30 15:11:10 +0100

    Remove ability to send email using Sendgrid
    Only Sendamatic (and console if DEBUG=True) now.
    
    Fixes #911

--- pepysdiary/config/settings.py
+++ pepysdiary/config/settings.py
@@ -300,15 +300,6 @@ if os.getenv("SENDAMATIC_USER_ID", default=""):
     EMAIL_HOST_PASSWORD = os.getenv("SENDAMATIC_PASSWORD")
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
-
-elif os.getenv("SENDGRID_USERNAME", default=""):
-    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-    EMAIL_HOST = "smtp.sendgrid.net"
-    EMAIL_HOST_USER = os.getenv("SENDGRID_USERNAME")
-    EMAIL_HOST_PASSWORD = os.getenv("SENDGRID_PASSWORD")
-    EMAIL_PORT = 587
-    EMAIL_USE_TLS = True
-
 elif DEBUG:
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
